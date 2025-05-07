828a6045d82b7d8cbf3d37fb974a5557b5a3170e
Marcelo Cruz <144838959+oMarceloSC@users.noreply.github.com>
2025-05-07 16:40:18 -0300

    Create main.java

---
+++ simulador/main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class simulador() {
+    public static void main(String[] args) {
+        System.out.println("Digite a quantidade de paginas: ");
+        String input = scanner.nextLine();
+
+        
+    }
+}
