114c639d127a37879d73f9a5b6d20885514e0a92
Marcelo Cruz <144838959+oMarceloSC@users.noreply.github.com>
2025-05-07 17:05:35 -0300

    simulador LRU

---
+++ simulador/simuladorLRU.java
@@ -0,0 +1,22 @@
+class simuladorLRU {
+    public static int simulador(List<Integer> pages, int frameCount) {
+        Set<Integer> frames = new HashSet<>();
+        Map<Integer, Integer> recentUse = new HashMap<>();
+        int faults = 0;
+        int time = 0;
+
+        for (int page : pages) {
+            if (!frames.contains(page)) {
+                faults++;
+                if (frames.size() == frameCount) {
+                    int lruPage = collections.min(recentUse.entrySet(), Map.Entry.comparingByValue()).getKey();
+                    frames.remove(lruPage);
+                    recentUse.remove(lruPage);
+                }
+                frames.add(page);
+            }
+            recentUse.put(page, time++);
+        }
+        return faults;
+    }
+}
